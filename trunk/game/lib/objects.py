5438603898bbd26b75acf4d9f2712e246f22189d
pymike93 <pymike93@58a2cc82-f555-0410-88ef-4329e38c7de9>
2008-09-07 22:44:33 +0000

    Moving is working

--- trunk/game/lib/objects.py
+++ trunk/game/lib/objects.py
@@ -1,16 +1,23 @@
+import math, pygame
+
 class Ship:
     graphic = 'generic_ship' # Image series the game should use to render
 
-    def __init__(self):
+    def __init__(self, pos):
         self._alive = True
 
         self.hull_max = 50
         self.hull = self.hull_max
         self.crew_max = 50
         self.crew = self.crew_max
-        self.speed_max = 10
+        self.speed_max = 100
         self.speed = self.speed_max
         self.damage_multiplier = 1
+        self.distance_left = self.speed
+        
+        self.pos = list(pos)
+        self.moved = False
+        self.selected = False
 
         self.hold_capacity = 40 # Resources can only be this number total
         self.resources = Resources(0, 0, 0) # Start empty
@@ -23,6 +30,37 @@ class Ship:
             return False
         else:
             return True
+
+    def end_turn(self):
+        self.distance_left = self.speed
+        self.moved = False
+
+    def move(self):
+        """Moves the player to the mouse pos."""
+        if self.distance_left > 0:
+            pos, distance = self.get_mouse_pos()
+            self.pos = list(pos)
+            self.distance_left -= distance
+        else:
+            self.moved = True
+        
+    def get_mouse_pos(self):
+        """Returns the pos of the mouse minus the distance left
+        that the ship can go, and the distance you can move"""
+        mx, my = pygame.mouse.get_pos()
+        x = self.pos[0] - mx
+        y = self.pos[1] - my
+        angle = math.atan2(y, x)
+        mouse_angle = int(270.0 - (angle * 180.0)/math.pi)
+        pos = list(self.pos)
+        xdiff = abs(mx-pos[0])
+        ydiff = abs(my-pos[1])
+        distance = math.sqrt((xdiff**2)+(ydiff**2))
+        if distance > self.distance_left:
+            distance = self.distance_left
+        pos[0] += math.sin(math.radians(mouse_angle))*distance
+        pos[1] += math.cos(math.radians(mouse_angle))*distance
+        return pos, distance
 
 class Resources:
     def __init__(self, wood, string, crew):
