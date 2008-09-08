4c62b876668933ee7854687826d8514712338cc5
RoeBros <RoeBros@58a2cc82-f555-0410-88ef-4329e38c7de9>
2008-09-08 17:58:37 +0000

    modified data.py to allow you to run game.py directly again, or any file in lib actually, an dbe able to access data, without having to run from run_game.py.  Removed main.py as it is not needed.

--- trunk/game/lib/data.py
+++ trunk/game/lib/data.py
@@ -1,8 +1,13 @@
 import pygame
-import os
+import os, sys
+x, y = os.path.split(sys.path[0])
+if y == "lib":
+    data_path = os.path.join(x, "data")
+else:
+    data_path = "data"
 
 all_images = {}
 def image(name):
     if name in all_images:
         return all_images[name]
-    return pygame.image.load(os.path.join("data", name)).convert_alpha()
+    return pygame.image.load(os.path.join(data_path, name)).convert_alpha()
