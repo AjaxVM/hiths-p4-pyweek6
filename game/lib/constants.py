1326a5cd193556384c819621f33453d03919b03e
xer.pixelarttut <xer.pixelarttut@58a2cc82-f555-0410-88ef-4329e38c7de9>
2008-09-12 17:36:18 +0000

    Early version of the name generater :D

--- game/lib/constants.py
+++ game/lib/constants.py
@@ -5,3 +5,9 @@ player_colors = [(255, 0, 0),
                (0, 0, 255),
                (255, 255, 0),
                (0, 255, 255)]
+
+territory_first_name = [("Name1"),
+                ("Name2")]
+
+territory_second_name = [("Name1"),
+                ("Name2")]
