09e696e354138f3901606df19747b7665923bdb9
xer.pixelarttut <xer.pixelarttut@58a2cc82-f555-0410-88ef-4329e38c7de9>
2008-09-12 19:07:21 +0000

    Updated names

--- game/lib/constants.py
+++ game/lib/constants.py
@@ -6,8 +6,13 @@ player_colors = [(255, 0, 0),
                (255, 255, 0),
                (0, 255, 255)]
 
-territory_first_name = [("Name1"),
-                ("Name2")]
+territory_first_name = [("Blackeyes "),
+                ("Pennies "),
+                ("Hookhands "),
+                ("My ")]
 
-territory_second_name = [("Name1"),
-                ("Name2")]
+territory_second_name = [("Cave"),
+                ("Hide"),
+                ("Hideout"),
+                ("Hold"),
+                ("Bedroom")]
